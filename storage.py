50c11b6f3b01cef59a65ead78c6953e2a52d886f
KiRiLl-glitch54 <kukuninkirill2011@gmail.com>
2026-05-17 15:14:47 +0300

    Update storage.py
    дописал

--- storage.py
+++ storage.py
@@ -5,19 +5,40 @@ from typing import List, Optional
 FILE_PATH = 'books.json'
 
 def _load_books() -> List[Book]:
-  pass
+    try:
+        with open(FILE_PATH, 'r', encoding='utf-8') as f:
+            data = json.load(f)
+            return [Book.from_dict(item) for item in data]
+    except FileNotFoundError:
+        return []
+    except json.JSONDecodeError:
+        print(f"Предупреждение: {FILE_PATH} пуст или содержи невалидный JSON. Начинаю с пустого листа.")
+        return []
 
 def _save_books(books: List[Book]):
-    pass
+    with open(FILE_PATH, 'w', encoding='utf-8') as f:
+        json.dump([book.to_dict() for book in books], f, ensure_ascii=False, indent=4)
 
 def add_book(book: Book):
-    pass
+    books = _load_books()
+    books.append(book)
+    _save_books(books)
+    print(f"Книга под названием '{book.title}' под авторством {book.author} добавлена.")
 
 def get_all_books() -> List[Book]:
-    pass
+    return _load_books()
 
 def remove_book(title: str) -> bool:
-    pass
+    books = _load_books()
+    initial_len = len(books)
+    books = [book for book in books if book.title.lower() != title.lower()]
+    if len(books) < initial_len:
+        _save_books(books)
+        print(f"Книга под названием '{title}' была удалена.")
+        return True
+    print(f"Книга под названием '{title}' не найдена.")
+    return False
 
 def find_books_by_author(author: str) -> List[Book]:
-    pass
+    books = _load_books()
+    return [book for book in books if book.author.lower() == author.lower()]
