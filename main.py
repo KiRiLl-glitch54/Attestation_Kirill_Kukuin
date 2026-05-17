cee26a581ae0439875d092e8544aff95ddaedd33
KiRiLl-glitch54 <kukuninkirill2011@gmail.com>
2026-05-17 12:47:05 +0300

    Update main.py
    решил маленько переделать и добавил отображение меню

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
+import datetime
+
+
 def display_menu():
+    print("\n--- Book Management App ---")
+    print("1. Добавить книгу")
+    print("2. Показать все книги")
+    print("3. Показать среднюю оценку")
+    print("4. Статистика по авторам")
+    print("5. Удалить книгу")
+    print("6. Выход")
+    print("-------------------------")
+
+def add_new_book():
+    pass
+
+def show_all_books():
+    pass
+
+def show_average_rating():
+    pass
+
+def show_author_statistics():
+    pass
+
+def delete_book():
     pass
 
 def main():
     pass
 
-if __name__ == "__main__":
-    main()
+main()
