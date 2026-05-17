66afbe06600ecd82c0889a2bd30915d178db6b0b
KiRiLl-glitch54 <kukuninkirill2011@gmail.com>
2026-05-17 13:06:25 +0300

    Create storage.py
    заготовка для "хранилища" код как-нибудь допишете

---
+++ storage.py
@@ -0,0 +1,23 @@
+import json
+from typing import List, Optional
+
+
+FILE_PATH = 'books.json'
+
+def _load_books() -> List[Book]:
+  pass
+
+def _save_books(books: List[Book]):
+    pass
+
+def add_book(book: Book):
+    pass
+
+def get_all_books() -> List[Book]:
+    pass
+
+def remove_book(title: str) -> bool:
+    pass
+
+def find_books_by_author(author: str) -> List[Book]:
+    pass
